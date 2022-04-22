d033da477b30a44a838378cdf1c437244ccf0fe9
Duong Jonathan <djonathan91.77@gmail.com>
2022-04-22 19:09:18 +0200

    correction des données pour Preview

--- MyNBAStore/Model/ProductModel.swift
+++ MyNBAStore/Model/ProductModel.swift
@@ -16,7 +16,7 @@ struct Product: Identifiable, Decodable {
     let price: Double
     let imageName: String
     
-    static let exemple = Product(id: 1, type: .tshirt, name: "LeBron James Lakers Icon Edition 2020", shortDescription: "Maillot Nike NBA Swingman", description: "Le maillot Icon Edition incarne le riche héritage de la franchise, ainsi que son identité emblématique, à travers les couleurs audacieuses de l'équipe.Le maillot Nike NBA Swingman Icon Edition des Lakers de Los Angeles s'inspire de la tenue des joueurs professionnels.Il présente une conception classique en tissu à double maille premium et une coupe qui met en valeur les supporters.Ce produit est entièrement fabriqué à partir de fibres de polyester recyclé.", price: 99.99, imageName: "lebronIconic")
+    static let exemple = Product(id: 1, type: .tshirt, name: "LeBron James Lakers Icon Edition 2020", shortDescription: "Maillot Nike NBA Swingman", description: "Le maillot Icon Edition incarne le riche héritage de la franchise, ainsi que son identité emblématique, à travers les couleurs audacieuses de l'équipe. Le maillot Nike NBA Swingman Icon Edition des Lakers de Los Angeles s'inspire de la tenue des joueurs professionnels. Il présente une conception classique en tissu à double maille premium et une coupe qui met en valeur les supporters. Ce produit est entièrement fabriqué à partir de fibres de polyester recyclé.", price: 99.99, imageName: "lebronIconic")
 }
 
 enum ProductType: Decodable {
