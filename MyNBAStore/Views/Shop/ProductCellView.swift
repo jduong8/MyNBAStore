c4ac2829a6571bce7bdf197da459ba2f6a0e65d7
Kevin Malkic <kevin@saltandpeppercode.com>
2022-04-22 21:57:11 +0100

    fix

--- MyNBAStore/Views/Shop/ProductCellView.swift
+++ MyNBAStore/Views/Shop/ProductCellView.swift
@@ -15,23 +15,35 @@ struct ProductCellView: View {
             Image(product.imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            VStack (alignment: .leading){
+            VStack (alignment: .leading, spacing: 4) {
                 Text(product.name)
                     .fontWeight(.heavy)
                     .foregroundColor(.black)
+                    .frame(maxWidth: .infinity)
                 Text(product.shortDescription)
                     .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity)
                 Text("6 Tailles")
                     .foregroundColor(.gray)
+                    .frame(maxWidth: .infinity)
+                Spacer()
                 Text("\(product.price, specifier: "%.2f") €")
                     .foregroundColor(.black)
+                    .frame(maxWidth: .infinity)
             }
+            .multilineTextAlignment(.center)
         }
     }
 }
 
 struct ProductCellView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductCellView(product: Product(id: 1, type: .tshirt, name: "LeBron James Lakers Icon Edition 2020", shortDescription: "Maillot Nike NBA Swingman", description: "", price: 99.99, imageName: "lebronIconic"))
+        LazyVGrid(columns: [
+            GridItem(.flexible(), spacing: 15, alignment: .top),
+            GridItem(.flexible(), spacing: 15, alignment: .top)
+        ], spacing: 15) {
+            ProductCellView(product: Product(id: 1, type: .tshirt, name: "LeBron James Lakers Icon Edition 2020 Edition 2020", shortDescription: "Maillot Nike NBA Swingman Nike NBA Swingman", description: "", price: 99.99, imageName: "lebronIconic"))
+            ProductCellView(product: Product(id: 1, type: .tshirt, name: "LeBron James Lakers Icon Edition 2020", shortDescription: "Maillot Nike NBA Swingman", description: "", price: 99.99, imageName: "lebronIconic"))
+        }
     }
 }
